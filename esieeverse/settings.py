98e369741d8436ab2a5faa32a07f0ab0eab76c74
DaSilvaRemi <remi.dasilva.dev@gmail.com>
2022-11-18 12:56:21 +0100

    feat: cgabge settings

--- esieeverse/settings.py
+++ esieeverse/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'esieeverse.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'remidasilva_esieeverse',
+        'USER': '220877_admin',
+        'PASSWORD': 'admindjangoBDD',
+        'HOST': 'mysql-remidasilva.alwaysdata.net',
+        'PORT' : '3306'
     }
 }
 
@@ -103,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
 TIME_ZONE = 'UTC'
 
