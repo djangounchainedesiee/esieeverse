622d19280b292d28dd87d81902710ed6dd9f1511
DaSilvaRemi <remi.dasilva.dev@gmail.com>
2023-01-13 21:31:52 +0100

    api

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,15 @@
 from django.urls import include, path
 from rest_framework import routers
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
-router = routers.DefaultRouter()
-router.register(r'messageList', views.MessageListView)
+#router = routers.DefaultRouter()
+# router.register(r'messageList', views.MessageList.get)
+# path('', include(router.urls))
 
 urlpatterns = [
-    path('', include(router.urls)),
+    #path('', include(router.urls)),
+    path('messageList/<int:conversation_id>', views.MessageList.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
