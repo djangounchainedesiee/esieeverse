c0cec96dd83f23b223a8b3224d66097332d471bc
Ilyes <ilyes.belkadi@edu.esiee.fr>
2023-01-15 13:42:21 +0100

    Update form

--- authentification/forms.py
+++ authentification/forms.py
@@ -1,22 +1,17 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from esieeverse.models import Utilisateur
-from esieeverse.models import Promotion
-from esieeverse.models import Classe
-from esieeverse.models import Fillière
-
-
 class SignUpForm(forms.Form):
+
+    Prénom = forms.CharField(label='Prénom', max_length=150)
+
+    Nom = forms.CharField(label='Nom ', max_length=150)
 
     email = forms.EmailField(max_length=254, help_text = 'Requis. Entrez une adresse mail valide.')
 
-    Nom = forms.CharField(
-        label='Nom de famille de l\'utilisateur', max_length=150)
-
-    Prénom = forms.CharField(
-        label='Prénom de l\'utilisateur', max_length=150)
-
-    
-
     password = forms.CharField(widget=forms.PasswordInput())
 
+    CHOICES = [('1','E1'),('2','E2'),('3','E3'),('4','E4'),('5','E5') ]
+    ma_promotion= forms.ChoiceField(choices=CHOICES)
+
+    
+ 
