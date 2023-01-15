d4170b2eb9c4f3161bd54c3f98491cc425251aa6
AyoubBEsiee <ayoub.benheddi@edu.esiee.fr>
2023-01-15 15:21:17 +0100

    Ayoub label_id

--- authentification/forms.py
+++ authentification/forms.py
@@ -9,9 +9,23 @@ class SignUpForm(forms.Form):
     email = forms.EmailField(max_length=254, help_text = 'Requis. Entrez une adresse mail valide.')
 
     password = forms.CharField(widget=forms.PasswordInput())
+    password_confirm = forms.CharField(widget=forms.PasswordInput)
+
+    def clean_password_confirm(self):
+        password = self.cleaned_data.get("password")
+        password_confirm = self.cleaned_data.get("password_confirm")
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError("Les mots de passe ne correspondent pas.")
+        return password_confirm
 
     CHOICES = [('1','E1'),('2','E2'),('3','E3'),('4','E4'),('5','E5') ]
-    ma_promotion= forms.ChoiceField(choices=CHOICES)
+    ma_promotion= forms.ChoiceField(choices=CHOICES, initial='1')
 
-    
- 
+    Cursus_Type = (
+        ('apprentissage', 'Apprentissage'),
+        ('temps plein', 'Temps Plein'),
+    )
+    cursus = forms.ChoiceField(widget=forms.RadioSelect, choices=Cursus_Type)
+
+
+ # 
