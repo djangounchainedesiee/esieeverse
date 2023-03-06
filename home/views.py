63756d08f1f3d85a0065c659a6e6815b7918d80a
DaSilvaRemi <remi.dasilva.dev@gmail.com>
2023-03-06 18:45:42 +0100

    refact: views

--- home/views.py
+++ home/views.py
@@ -5,7 +5,7 @@ from publication.models import Publication
 def home_view(request: HttpRequest) -> HttpResponse:
     # Récupère les publications selon les abonnements de l'utilisateur connecté
     publications = Publication.objects.filter(auteur_id__in=request.user.utilisateur.abonnements.all()).exclude(auteur_id=request.user.utilisateur)
-
+    
     context = {
         'publications': publications,
     }
