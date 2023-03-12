a372594e99c4cc5edfa9f0984b36b80ea3700406
dasilvar <remi.dasilva@edu.esiee.fr>
2023-03-12 18:13:11 +0100

    fix: correction de l'affichage de l'utilisateur connecte sur la fenêtre de création de conversation

--- esieechat/forms.py
+++ esieechat/forms.py
@@ -9,10 +9,13 @@ class ConversationUtilisateursForm(forms.Form):
     """
     Représente un fomulaire perttant de créer une conversation en choisisant ses utilisations et son nom
     """
-    nom_conversation = forms.CharField(
-        label='Nom de la conversation', max_length=20)
-    utilisateurs = forms.ModelMultipleChoiceField(
-        label='Choisissez des utilisateurs', queryset=Utilisateur.objects.filter(user__in=User.objects.filter(groups__name='etudiants')))
+    def __init__(self, *args, **kwargs):
+        utilisateur_connecte = kwargs.pop('utilisateur_connecte')
+        super().__init__(*args, **kwargs)
+        self.fields['utilisateurs'].queryset = Utilisateur.objects.all().exclude(id=utilisateur_connecte.id)
+
+    nom_conversation = forms.CharField(label='Nom de la conversation', max_length=20)
+    utilisateurs = forms.ModelMultipleChoiceField(label='Choisissez des utilisateurs', queryset=Utilisateur.objects.none())
 
 class MessageForm(forms.ModelForm):
     """
