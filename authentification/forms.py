d9599dc89d704ba2ee96e96d393612ed54fc882a
Ilyes <ilyes.belkadi@edu.esiee.fr>
2023-03-17 02:47:09 +0100

    Ajout pdp et nv champs

--- authentification/forms.py
+++ authentification/forms.py
@@ -1,13 +1,16 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+
 class SignUpForm(forms.Form):
+
+    profile_picture = forms.ImageField(required=False, widget=forms.ClearableFileInput())
 
     Prénom = forms.CharField(label='Prénom', max_length=150)
 
     Nom = forms.CharField(label='Nom', max_length=150)
 
     email = forms.EmailField(max_length=254, help_text = 'Requis. Entrez une adresse mail valide.')
-
+    
     password = forms.CharField(widget=forms.PasswordInput())
     password_confirm = forms.CharField(widget=forms.PasswordInput)
 
@@ -27,8 +30,11 @@ class SignUpForm(forms.Form):
     )
     cursus = forms.ChoiceField(widget=forms.RadioSelect, choices=Cursus_Type)
 
-    CHOICES = [('FR','Réseaux et sécurité'),('FE','Systèmes embarqués'),('FI','Informatique et applications'),('FG','Génie industriel'),('FT','Energies') ]
-    ma_filliere= forms.ChoiceField(choices=CHOICES, initial='1')
+    CHOICES_app = [('FR','Réseaux et sécurité'),('FE','Systèmes embarqués'),('FI','Informatique et applications'),('FG','Génie industriel'),('FT','Energies') ]
+    ma_filliere_app= forms.ChoiceField(choices=CHOICES_app, initial='1')
 
+    CHOICES_tp = [('INF','Informatique'),('CYB','Cybersécurité'),('DSIA','Datascience et intelligence artificielle'),('AIC','Artificial intelligence and cybersecurity'),('SE','Systèmes embarqués'), ('SEI','Systèmes électroniques intelligents'),('GI','Génie industriel'),('BIO','Biotechnologies et e-Santé'),('ENE','Energie')]
+    ma_filliere_tp= forms.ChoiceField(choices=CHOICES_tp, initial='1')
 
- # 
+    entreprise = forms.CharField(max_length=100,  required=False, label="Nom de l'entreprise")
+  
--- authentification/views.py
+++ authentification/views.py
@@ -5,16 +5,17 @@ from django.contrib.auth import login, authenticate
 from django.contrib import messages
 
 def register(request):
-	if request.method == 'POST' :
-		form = SignUpForm(request.POST)
-		if form.is_valid():
-			form.save()		
-			username = form.cleaned_data.get('username')
-			password = form.cleaned_data.get('password1')
-			user = authenticate(username=username, password=password)
-			login(request,user)	
-			messages.success(request, f'Coucou {username}, Votre compte a été créé avec succès !')					
-			return redirect('accounts')
-	else :
-		form = SignUpForm()
-	return render(request,'registration/register.html',{'form' : form})
+    if request.method == 'POST':
+        form = SignUpForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.profile_picture = form.cleaned_data.get('profile_picture')
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
+            messages.success(request, f'Hello {user.username}, Your account has been created successfully!')
+            return redirect('accounts')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/register.html', {'form': form})
