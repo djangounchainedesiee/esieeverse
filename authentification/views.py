f59965733dc77861bb8dc3dda9bd28845afb3496
AyoubBEsiee <ayoub.benheddi@edu.esiee.fr>
2023-06-05 22:03:39 +0200

    commit reglage bdd

--- authentification/views.py
+++ authentification/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView
 #from django.contrib.auth.forms import UserCreationForm
@@ -14,20 +14,47 @@ def signup(request):
         if form.is_valid():
             # Créer un nouvel utilisateur
             user = User.objects.create_user(
-                username=form.cleaned_data['email'],
+                first_name=form.cleaned_data['Prénom'],
+                last_name = form.cleaned_data['Nom'],
+                username=form.cleaned_data['username'],
                 email=form.cleaned_data['email'],
                 password=form.cleaned_data['password']
             )
 
+
             # Ajouter les autres données d'inscription à l'utilisateur
-            user.first_name = form.cleaned_data['Prénom']
-            user.last_name = form.cleaned_data['Nom']
-            user.profile_picture = form.cleaned_data['profile_picture']
-            user.ma_promotion = form.cleaned_data['ma_promotion']
-            user.cursus = form.cleaned_data['cursus']
-            user.ma_filliere_app = form.cleaned_data['ma_filliere_app']
-            user.ma_filliere_tp = form.cleaned_data['ma_filliere_tp']
-            user.entreprise = form.cleaned_data['entreprise']
+
+
+            cursus = form.cleaned_data['cursus']
+
+            if(cursus=="apprentissage"):
+                filiere_nom = form.cleaned_data['ma_filliere_app']
+
+            if(cursus=="temps plein"):
+                filiere_nom = form.cleaned_data['ma_filliere_tp']
+
+
+            filiere = get_object_or_404(Filiere, nom=filiere_nom)  
+            #si mon cursus est app alors filliere-> ma filiere app
+            
+
+            promotion_id = form.cleaned_data['ma_promotion']
+            promotion = get_object_or_404(Promotion, nom=promotion_id)  # Récupérer l'instance de Promotion correspondante
+
+
+            utilisateur = Utilisateur.objects.create(
+                prenom = form.cleaned_data['Prénom'],
+                nom = form.cleaned_data['Nom'],
+                user=user,
+                promotion= promotion,
+                filiere=filiere,
+                photo_de_profile=form.cleaned_data['profile_picture'],
+                entreprise=form.cleaned_data['entreprise']
+            )
+
+
+            utilisateur.save()
+
             user.save()
 
             # Rediriger vers la page de connexion
