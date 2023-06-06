c8a4f8d6cec880afd855cfb012341416861a8933
Wahib93290 <93159370+Wahib93290@users.noreply.github.com>
2023-06-06 22:13:15 +0200

    Correction de la configuration

--- profilSetting/forms.py
+++ profilSetting/forms.py
@@ -1,14 +1,43 @@
 from django import forms
 
 class SettingForm(forms.Form):
+    first_name = forms.CharField(
+        label="Prénom",
+        max_length=50,
+        required = False,   
+        widget=forms.TextInput()
+    )
+
+    last_name = forms.CharField(
+        label="Nom",
+        max_length=50,
+        required = False,   
+        widget=forms.TextInput()
+    )
+
+    login = forms.CharField(
+        label="Login",
+        max_length=50,
+        required = False,   
+        widget=forms.TextInput()
+    )
+
+    email = forms.CharField(
+        label="Adresse mail",
+        max_length=50,
+        required = False,   
+        widget=forms.TextInput()
+    )
+
+    photo_de_profile = forms.ImageField(
+        required=False,    
+        label="Photo de Profile",
+        widget=forms.FileInput(attrs={'class': 'form-control', 'type': 'file'})
+    )
+
     password = forms.CharField(
         label="Mot de passe",
         max_length=50,
         required = False,   
         widget=forms.PasswordInput(attrs={'class': 'form-control'})
-    )
-    photo_de_profile = forms.ImageField(
-        required=False,    
-        label="Photo de Profile",
-        widget=forms.FileInput(attrs={'class': 'form-control', 'type': 'file'})
     )
--- profilSetting/views.py
+++ profilSetting/views.py
@@ -19,14 +19,30 @@ def profil_setting(request: HttpRequest) -> HttpResponse:
         if setting_form.is_valid():
             password = setting_form.cleaned_data['password']
             photo_de_profile = setting_form.cleaned_data['photo_de_profile']
+            first_name = setting_form.cleaned_data['first_name']
+            last_name = setting_form.cleaned_data['last_name']
+            login = setting_form.cleaned_data['login']
+            email = setting_form.cleaned_data['email']
 
             if password is not None and len(password) > 0:
                 utilisateur.user.set_password(password) 
-                utilisateur.user.save()
+
+            if first_name is not None and len(first_name) > 0:
+                utilisateur.user.first_name = first_name 
+
+            if last_name is not None and len(last_name) > 0:
+                utilisateur.user.last_name = last_name 
+
+            if login is not None and len(login) > 0:
+                utilisateur.user.username = login 
+
+            if email is not None and len(email) > 0:
+                utilisateur.user.email = email 
                     
             if photo_de_profile is not None:
                 utilisateur.photo_de_profile = photo_de_profile
             
+            utilisateur.user.save()
             utilisateur.save()
 
             return redirect("profil:view_profil", id_utilisateur = utilisateur.id)
